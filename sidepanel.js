220127d8153067b64a0420edf86af3b7f0368c16
Lakshmana <lakshmana.vissarapu@gmail.com>
2026-06-12 12:07:39 +0530

    Removed the serve file

--- sidepanel.js
+++ sidepanel.js
@@ -251,7 +251,7 @@ SELECTORS
 ${JSON.stringify(selectors, null, 2)}
 `;
 
-    const API_KEY = ""; // 🔥 API Key
+    const API_KEY = process.env.API_KEY; // 🔥 API Key : AQ.Ab8RN6J2R4uubu7udjDvIxfC5KmrOoC9_t7TrKAVkMG5OVAQTQ
 
     const response = await fetch(
   `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`,
