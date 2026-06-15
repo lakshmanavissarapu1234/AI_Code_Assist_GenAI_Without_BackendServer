6c183b8970ad099a6f396b9db6388881b80c0ec2
lakshmanavissarapu1234 <lakshmana.vissarapu@gmail.com>
2026-06-15 11:46:06 +0530

    Update sidepanel.js
    removed api_key

--- sidepanel.js
+++ sidepanel.js
@@ -120,7 +120,7 @@ Use iframeOuterHTML only for FrameLocator generation.
 ${JSON.stringify(selectors, null, 2)}
 `;
 
-    const API_KEY = "AQ.Ab8RN6JvIsrJCYG9tHuJq6xWI63XIwwcqezTUhnyw12NfvUckg"; // 🔥 API Key
+    const API_KEY = ""; // 🔥 API Key
 
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`,
